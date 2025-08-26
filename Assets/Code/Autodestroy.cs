1d37eee0fcff33b12bb5bf0145c87261162b9d67
Went <bavuong765@gmail.com>
2025-08-27 03:11:59 +0700

    ///

---
+++ Assets/Code/Autodestroy.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class DestroyGameObject : MonoBehaviour
+{
+    void Start()
+    {
+        // Tự động hủy game object sau 3 giây
+        Destroy(gameObject, 3f);
+    }
+}
